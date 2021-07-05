095617c8ecadbeae40aa791be328c0f8ffb13256
Stefanie Fissneider <stefanie.fissneider@infotray.com>
2021-07-05 13:32:38 +0200

    Erstes Hello World CPP Programm

---
+++ beispielAbgucken/beispielAbgucken.cpp
@@ -0,0 +1,12 @@
+﻿// beispielAbgucken.cpp : Defines the entry point for the application.
+//
+
+#include "beispielAbgucken.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!" << endl;
+	return 0;
+}
